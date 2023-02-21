b26285fd33730266528ba81a73fb6586502adad8
hawm <20796940+hawm@users.noreply.github.com>
2023-02-21 13:54:37 +0000

    update user format restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,10 @@ class User < ApplicationRecord
   attr_accessor :skip_username_validations
 
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 10 },
-                       format: { with: /[a-z]/, message: 'only allows letters' },
+                       format: { with: /[a-z\d]/, message: 'only allows letters' },
                        unless: :skip_username_validations
   before_validation { self.username = username.downcase }
+
+  has_many :bookmarks
+  has_many :books, through: :bookmarks
 end
